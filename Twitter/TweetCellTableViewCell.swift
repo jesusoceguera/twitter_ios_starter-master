e3935ba64d3a217abeb2aa39c7fce74ccaf1f205
JesusOcegueraPena <jo1138241@hotmail.com>
2020-02-20 20:37:53 -0800

    Initial Commit

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,84 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by JESUS OCEGUERA-PENA on 2/18/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    @IBOutlet weak var retweetButton: UIButton!
+    
+    @IBOutlet weak var heartButton: UIButton!
+    
+
+    
+    var favorited:Bool = false
+    var tweetId: Int = -1
+    
+    func setFavorite(_ isFavorited:Bool) {
+        favorited = isFavorited
+        if (favorited) {
+            heartButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        }
+        else {
+            heartButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)        }
+    }
+    
+    @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: {(error) in
+            print("Error is retweeting: \(error)")
+        })
+        
+    }
+    func setRetweeted(_ isRetweeted:Bool){
+        if (isRetweeted){
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        } else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true        }
+    }
+    
+    
+    @IBAction func heartedButton(_ sender: Any) {
+       let toBeFavorited = !favorited
+        if (toBeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: { self.setFavorite(true)
+            }, failure: {(error) in
+                print("Favorite did not succeed: \(error)")
+            })
+        } else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: { self.setFavorite(false)
+            }, failure: {(error) in
+                print("Unfavorite did not succeed: \(error)")
+            })
+        }
+        
+        
+    }
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
